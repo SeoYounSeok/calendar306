845a4565b8368344d507832fd633ead66cd7a166
SeoYounSeok <syounseok94@gmail.com>
2022-01-05 23:22:14 +0900

    # 2022-01-05
    스케줄 설명 추가

---
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -0,0 +1,10 @@
+import { TextInput } from "react-native-paper";
+
+
+const CustomTextInput = () => {
+    return (
+        <TextInput />
+    );
+}
+
+export default CustomTextInput;
--- src/screens/Schedule.tsx
+++ src/screens/Schedule.tsx
@@ -42,13 +42,4 @@ const Schedule = () => {
   );
 };
 
-// const styles = StyleSheet.create({
-//   input: {
-//     height: 40,
-//     margin: 12,
-//     borderWidth: 1,
-//     padding: 10,
-//   },
-// });
-
 export default Schedule;
