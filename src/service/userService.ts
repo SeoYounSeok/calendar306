6e72e7e71d16f2769ee909896360348ba87c14d9
SeoYounSeok <syounseok94@gmail.com>
2022-01-18 13:10:34 +0900

    #2022-01-18
    회원가입 항목 추가

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,14 +1,16 @@
 import API from '../utils/Api';
 
 type userProps = {
+  userId: string;
   name: string;
-  password?: string;
+  password: string;
   address: string;
 };
 
 export const registerService = async ({...props}: userProps) => {
   await API.post('/users/create', {
     params: {
+      userId: props.userId,
       name: props.name,
       password: props.password,
       address: props.address,
