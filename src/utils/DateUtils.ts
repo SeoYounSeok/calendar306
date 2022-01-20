8f97c0c3131db1ce06e7fc89098bc81db7c6e3f2
SeoYounSeok <syounseok94@gmail.com>
2022-01-20 10:46:44 +0900

    # 2022-01-20
    Date 함수 2개 추가, 날짜 구하는 간단 함수

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -18,3 +18,22 @@ export const setWeeklyCalendar = (month: any) => {
   }
   return weeklyDays;
 };
+
+export const convertDateFormat = (initDate : Date) => {
+  const dayName: string[] = ['일', '월', '화', '수', '목', '금', '토'];
+  var year = initDate.getFullYear();
+  var month : string | number = initDate.getMonth() + 1;
+  var date : string | number = initDate.getDate();
+  var day = dayName[initDate.getDay()];
+  month = month >= 10 ? month : '0' + month;
+  date = date >= 10 ? date : '0' + date;  
+  return `${year}.${month}.${date}(${day})`
+}
+
+export const convertTimeFormat = (initDate : Date) => {
+  var hours = initDate.getHours();
+  var minutes = initDate.getMinutes();
+  var meridiem = hours < 12 ? '오전' : '오후';
+  return `${meridiem} ${hours}:${minutes}`; 
+}
+
