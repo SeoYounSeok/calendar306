cf7939a1f87d6d17194d100c22c5df7f742f0827
SeoYounSeok <syounseok94@gmail.com>
2022-01-25 18:05:02 +0900

    #2022-01-25
    스케줄 모델 추가

---
+++ src/model/schedules.js
@@ -0,0 +1,18 @@
+export defualt Schedules = [
+    {
+        "userId":"soso",
+        "calendarType" : "simple",
+        "directoryId": 1,
+        "title":"test title",
+        "content": "test Content",
+        "isAllday" : true,
+        "startDate" :"2022-01-01",
+        "endDate" : "2022-01-20",
+        "location" : "seoul",
+        "attendant" : "jinwoo",
+        "isReminder" : false,
+        "reminderValue": "알림 10분 전",
+        "isRecurrence" : false,
+        "recurrenceValue": "주말마다",
+    },
+];
